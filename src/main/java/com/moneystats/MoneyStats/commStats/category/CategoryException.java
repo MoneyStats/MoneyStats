49a69ec064a59500f93328b8e9f4b6c412a5db1b
giovannilamarmora <giovannilamarmora.working@gmail.com>
2021-08-30 16:46:57 +0200

    Added CategoryException.java, CategoryExceptionMapper.java to handle Exception

---
+++ src/main/java/com/moneystats/MoneyStats/commStats/category/CategoryException.java
@@ -0,0 +1,23 @@
+package com.moneystats.MoneyStats.commStats.category;
+
+public class CategoryException extends Exception{
+
+    private final Code code;
+
+    /**
+     * Constructs a new exception with {@code null} as its detail message.
+     * The cause is not initialized, and may subsequently be initialized by a
+     * call to {@link #initCause}.
+     */
+    public CategoryException(Code code) {
+        this.code = code;
+    }
+
+    public Code getCode() {
+        return code;
+    }
+
+    public static enum Code {
+        CATEGORY_NOT_FOUND
+    }
+}
