c4d2f18daf60c32de2428cdf7f6da760d754a03b
giovannilamarmora <83874719+giovannilamarmora@users.noreply.github.com>
2021-10-13 23:57:16 +0200

    Change password

--- src/main/resources/static/mobile/assets/js/settings.js
+++ src/main/resources/static/mobile/assets/js/settings.js
@@ -62,6 +62,7 @@ $(document).ready(function () {
     $('#updateEmail').click(function () {
         updateUser();
     });
+
     function updateUser() {
         editMode = true;
         const authCredentialToUpdateDTO = {
@@ -88,6 +89,7 @@ $(document).ready(function () {
             })
         }
     }
+
     //-------------------------------------------------------------
     // END On click of Update user it will take all the information need it
     //-------------------------------------------------------------
@@ -109,7 +111,8 @@ $(document).ready(function () {
                 Swal.fire({
                     title: '<span style="color:#2D2C2C">Edited!</span>',
                     icon: 'success',
-                showConfirmButton: false})
+                    showConfirmButton: false
+                })
                 setTimeout(function () {
                     window.location.href = 'index.html';
                 }, 1000);
@@ -124,4 +127,87 @@ $(document).ready(function () {
         });
         localStorage.removeItem('username');
     }
+
+    //-------------------------------------------------------------
+    // Update Password User
+    //-------------------------------------------------------------
+    const PASSWORD_NOT_MATCH = "PASSWORD_NOT_MATCH";
+    const WRONG_CREDENTIAL = "WRONG_CREDENTIAL";
+
+    $('#updatePassBtn').click(function () {
+        editMode = true;
+        const authChangePasswordInputDTO = {
+            username: localStorage.getItem('username'),
+            oldPassword: $('#oldPassword').val(),
+            newPassword: $('#newPassword').val(),
+            confirmNewPassword: $('#newPasswordCheck').val()
+        }
+        if (editMode) {
+            Swal.fire({
+                icon: 'question',
+                title: '<span style="color:#2D2C2C">Do you want to save the current Password?</span>',
+                showDenyButton: true,
+                showCancelButton: true,
+                confirmButtonText: `Save`,
+                denyButtonText: `Don't Save`,
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    changePassword(authChangePasswordInputDTO);
+                } else if (result.isDenied) {
+                    Swal.fire('<span style="color:#2D2C2C">Operation Aborted!</span>', '', 'info')
+                }
+            })
+        }
+    })
+
+    function changePassword(authChangePasswordInputDTO) {
+        $.ajax({
+            type: "PUT",
+            url: `/credential/update/password`,
+            data: JSON.stringify(authChangePasswordInputDTO),
+            contentType: 'application/json',
+            dataType: 'json',
+            headers: {
+                Authorization: sessionStorage.getItem('accessToken')
+            },
+            success: function () {
+                editMode = false;
+                Swal.fire({
+                    title: 'Edited!',
+                    icon: 'success',
+                    showConfirmButton: false
+                })
+                setTimeout(function () {
+                    window.location.href = 'homepage.html';
+                }, 1000);
+            },
+            error: function (authErrorResponseDTO) {
+                var responseDTO = authErrorResponseDTO.responseJSON.message;
+                if (responseDTO === PASSWORD_NOT_MATCH) {
+                    Swal.fire({
+                        icon: 'error',
+                        title: '<span style="color:#2D2C2C">Error!</span>',
+                        text: "The password insert don't match"
+                    })
+                }
+                if (responseDTO === WRONG_CREDENTIAL) {
+                    Swal.fire({
+                        icon: 'error',
+                        title: '<span style="color:#2D2C2C">Error!</span>',
+                        text: "The password insert is not equal to the old password"
+                    })
+                } else {
+                    Swal.fire({
+                        icon: 'error',
+                        title: '<span style="color:#2D2C2C">Error!</span>',
+                        text: 'Process aborted, try again.'
+                    })
+                }
+            }
+        });
+    }
+
+    //-------------------------------------------------------------
+    // END Update Password User
+    //-------------------------------------------------------------
 });
